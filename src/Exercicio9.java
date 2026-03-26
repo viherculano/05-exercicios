a655b3c5ff43597ee07cd855eb1a0fe7c26c17ad
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 20:38:01 -0300

    exercicio 09 finalizado

--- src/Exercicio9.java
+++ src/Exercicio9.java
@@ -8,6 +8,9 @@ public class Exercicio9 {
         Scanner sc = new Scanner(System.in);
         int numero;
         int invertido;
+        int unidade;
+        int dezena;
+        int centena;
 
         // entrada de dados
 
@@ -15,6 +18,21 @@ public class Exercicio9 {
         numero = sc.nextInt();
 
         // processamento de dados
+
+        unidade = numero % 10;
+        dezena = (numero / 10) % 10;
+        centena = numero / 100;
+
+        invertido = unidade * 100 + dezena * 10 + centena;
+
+        // saída de dados
+
+        System.out.println("O número invertido é ---> " + invertido);
+
+
+
+
+
 
 
 
